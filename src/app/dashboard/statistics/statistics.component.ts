82dcdc504b98095b6be9fe7801ab962726fd0d88
Priyanshu Dhall <priyanshudhall2001@gmail.com>
2023-08-14 22:17:27 +0530

    new Dashboatd Design language

--- src/app/dashboard/statistics/statistics.component.ts
+++ src/app/dashboard/statistics/statistics.component.ts
@@ -16,7 +16,7 @@ export class StatisticsComponent implements OnInit {
   isCustomerChange: boolean = true;
   isOrderChange: boolean = false;
   isRevenueChange: boolean = true;
-  
+
   ngOnInit(): void {
     this.createBarChart();
     this.createDonut();
@@ -28,59 +28,105 @@ export class StatisticsComponent implements OnInit {
     // }); 
   }
 
-  createBarChart(){
+  createBarChart() {
+
     this.barChart = new Chart("barChart", {
-      type: 'bar',
+
+      type: 'line',
       data: {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
           label: "Sales",
           data: ["30", "40", "45", "50", "49", "60", "70", "91", "125", "70", "91", "125"],
-          backgroundColor: 'rgba(57, 62, 70, 0.5)',
-          maxBarThickness: 15
+          backgroundColor: 'rgba(57, 62, 70)',
+          borderColor: 'rgba(57, 62, 70)',
+          borderWidth: 2,
+          pointBorderColor: 'white'
         },
         {
           label: "Profit",
           data: ["50", "20", "60", "50", "90", "40", "60", "80", "250", "80", "123", "50"],
           backgroundColor: 'rgb(0, 173, 181)',
-          maxBarThickness: 15,
-          pointStyle: 'circle'
+          // maxBarThickness: 5,
+          borderColor: 'rgba(0, 173, 181)',
+          pointStyle: 'circle',
+          borderWidth: 3,
+          pointBackgroundColor: 'white'
+          // borderRadius: 5,
         }]
       },
       options: {
-        responsive: true
+        responsive: true,
+        elements: {
+          point:{
+            radius: 5,
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: {
+              boxWidth: 12,
+              boxHeight: 12,
+              padding: 24,
+              borderRadius: 200,
+            },
+          }
+        },
+        scales:{
+          x: {
+            grid: {
+              color: 'rgba(0, 0, 0, 0.08)',
+            },
+          },
+          y: {
+            grid: {
+              color: 'rgba(0, 0, 0, 0.0)',
+            },
+          }
+        }
+        
       }
+
     });
   }
-  
 
 
-  createDonut(){
+
+  createDonut() {
     this.donutChart = new Chart('donutChart', {
       type: 'doughnut',
 
       data: {
         labels: ['Satisfied', 'Dissatisfied', 'Neutral'],
         datasets: [{
-          data: ['75', '25', '50'],
+          data: ['80', '30', '40'],
           backgroundColor: [
             'rgba(0, 28, 48, 0.9)',
             'rgb(0, 173, 181)',
-            'rgba(57, 62, 70, 0.5)'
+            'rgba(57, 62, 70, 0.2)'
           ],
           hoverOffset: 10,
           borderWidth: 2,
           borderRadius: 5,
+          
         }]
 
-      }, 
+      },
       options: {
-        radius: 120,
+        // radius: 120,
         responsive: true,
+        cutout: 80,
         plugins: {
           legend: {
-            position: 'bottom'
-          }
+            position: 'bottom',
+            labels: {
+              boxWidth: 16,
+              boxHeight: 16,
+              padding: 18,
+              borderRadius: 200,
+            },
+          },
         }
       }
     });
