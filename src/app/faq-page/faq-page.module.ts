ed7709740e037ab28554785d5f38edf232b99e38
VIVEK1DHIMAN <googlydhiman.4236@gmail.com>
2023-08-16 10:41:05 +0530

    update

--- src/app/faq-page/faq-page.module.ts
+++ src/app/faq-page/faq-page.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaqPageComponent } from './faq-page.component';
 import { SharedModule } from '../shared/shared.module';
-<<<<<<< Updated upstream
-=======
-import { RouterModule, Routes } from '@angular/router';
-
-// const routes: Routes = [
-//   { path: 'faq', component: FaqPageComponent }
-// ];
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -18,11 +10,6 @@ import { RouterModule, Routes } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-<<<<<<< Updated upstream
-=======
-    // RouterModule.forChild(routes),
-
->>>>>>> Stashed changes
   ]
 })
 export class FaqPageModule { }
