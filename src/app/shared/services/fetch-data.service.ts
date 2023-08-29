d46c4102d5264b47fb3c160c10c5dc1d16cd82d8
davinder22133 <meetdavinderkumar@gmail.com>
2023-08-29 13:23:10 +0530

    added profile section

--- src/app/shared/services/fetch-data.service.ts
+++ src/app/shared/services/fetch-data.service.ts
@@ -7,9 +7,22 @@ import { Observable} from 'rxjs';
 })
 export class FetchDataService {
   url = '../../../assets/tempDB/products.json';
+  userUrl='../../../assets/tempDB/usersData.json'
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
     return this.http.get(this.url);
   }
+
+  getUserData() {
+    // let x;
+    //  const x=this.http.get(this.userUrl).toPromise();
+
+     return this.http.get(this.userUrl);
+      
+    
+  }
+
+
+
 }
