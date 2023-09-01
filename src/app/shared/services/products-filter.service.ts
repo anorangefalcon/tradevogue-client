0c89bac35b1f9fb66d1d8930445e1c2709a7d4ad
Jahnavi Singh <jahnavisingh0611@gmail.com>
2023-09-01 21:32:38 +0530

    filter ts

--- src/app/shared/services/products-filter.service.ts
+++ src/app/shared/services/products-filter.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { FetchDataService } from './fetch-data.service';
 
 @Injectable({
@@ -15,49 +15,39 @@ export class ProductsFilterService {
 
   ];
 
-  new_data: any = [];
 
   constructor(private fetchData: FetchDataService) { }
 
-  fetchUnique(): any {
+  async fetchUnique() {
 
-    this.fetchData.getData().subscribe((data) => {
+    const data = await this.fetchData.getData().toPromise();
 
-      console.log("inside ", data);
+    this.productData = data; // we will filter products in ProductData array
+    this.originalProductData = data //so that our original values doesnt change
 
-      this.productData = data; // we will filter products in ProductData array
-      this.originalProductData = data //so that our original values doesnt change
-      this.new_data = data;
-      console.log("this new data is ", this.new_data);
+    this.productData.forEach((product) => {
+      let category = product.info.category;
 
-      this.productData.forEach((product) => {
-        let category = product.info.category;
+      if (!this.uniqueValues[0].categories.includes(category)) {
+        this.uniqueValues[0].categories.push(category);
+      }
 
-        if (!this.uniqueValues[0].categories.includes(category)) {
-          this.uniqueValues[0].categories.push(category);
-        }
+      let brand = product.info.brand;
 
-        let brand = product.info.brand;
+      if (!this.uniqueValues[1].brands.includes(brand)) {
+        this.uniqueValues[1].brands.push(brand);
+      }
 
-        if (!this.uniqueValues[1].brands.includes(brand)) {
-          this.uniqueValues[1].brands.push(brand);
-        }
+      let color = product.colors;
+      this.uniqueValues[2].colors = this.uniqueValues[2].colors.concat(color);
 
-        let color = product.colors;
-        this.uniqueValues[2].colors = this.uniqueValues[2].colors.concat(color);
+    });
 
-      });
+    this.uniqueValues[2]['colors'] = this.filterColor(this.uniqueValues[2].colors);
 
-      this.uniqueValues[2]['colors'] = this.filterColor(this.uniqueValues[2].colors);
+    console.log("Unique Values: ", this.uniqueValues);
 
-      console.log(this.uniqueValues, "unique");
-
-    })
-    console.log("this unique data is ", this.uniqueValues);
-
-    console.log("this. orignal arrau ", this.new_data)
-
-    return { "data": this.new_data, "unique": this.uniqueValues };
+    return { "productData": this.originalProductData, "uniqueData": this.uniqueValues};
   }
 
   filterColor(data: any) {
@@ -70,8 +60,6 @@ export class ProductsFilterService {
       }
 
     })
-
-
     return data;
   }
 
