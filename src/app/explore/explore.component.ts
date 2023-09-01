5d70212d4219d34b5f9f7148bd7c85343347da53
Jahnavi Singh <jahnavisingh0611@gmail.com>
2023-09-01 21:32:38 +0530

    filter ts

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { FetchDataService } from '../shared/services/fetch-data.service';
 import { ProductsFilterService } from '../shared/services/products-filter.service';
+import { data } from 'jquery';
 
 @Component({
   selector: 'app-explore',
@@ -10,75 +10,76 @@ import { ProductsFilterService } from '../shared/services/products-filter.servic
 
 export class ExploreComponent {
 
-  productArr: any[] = [];
-  original_data: any[] = []; 
-  // colors:any=[];
-  unique:any=[];
-  categories: any[] = [];
-  
-  selectedCategory: String = '';
+  productData: any[] = [];
+  uniqueData: any[] = [];
 
-  constructor(private fetchData: FetchDataService,private myservice:ProductsFilterService) {
+  constructor(private productFilter: ProductsFilterService) {
 
-   let obj= this.myservice.fetchUnique();
-  // console,
-  this.productArr=obj.data;
-  this.unique=obj.unique;
+    this.getServiceData();
 
-  console.log("product is ",this.productArr);
-  console.log("uniique is ",this.unique);
-  
-  
   }
 
-  i = 0;
-  categoryChecked(el: any) {
+  async getServiceData() {
 
-    let type = el.target.value;
-    console.log(type);
-
-    if (el.target.checked){
-
-      if (this.i == 0) {
-        let filteredData = (this.original_data.filter((item) => {
-          return item.info.category == type
-        }));
-        this.productArr = filteredData;
-        this.i++;
-        return;
-      }
-  
-      let filteredData = (this.original_data.filter((item) => {
-        return item.info.category == type
-      })
-      )
-  
-      filteredData.map(item => {
-        this.productArr.push(item); 
-      })
-   
-
-    }
-    if (!el.target.checked) {
-      this.productArr = this.productArr.filter(item =>
-        item.info.category != type);
-    }
-    // console.log(this.productArr, "finalll");
-
-    if (this.productArr.length === 0)
-    {
-      this.productArr = this.original_data;
-      this.i = 0;
-    }
-  }
-
-  clear() {
-    this.productArr = this.original_data;
+    const data = await this.productFilter.fetchUnique();
     
+    this.productData = data.productData;
+    this.uniqueData = data.uniqueData;
+
+    console.log("Product Data", this.productData);
+    console.log("Unique Data", this.uniqueData);
     
     
   }
-  
+
+  // i = 0;
+  // categoryChecked(el: any) {
+
+  //   let type = el.target.value;
+  //   console.log(type);
+
+  //   if (el.target.checked){
+
+  //     if (this.i == 0) {
+  //       let filteredData = (this.original_data.filter((item) => {
+  //         return item.info.category == type
+  //       }));
+  //       this.productArr = filteredData;
+  //       this.i++;
+  //       return;
+  //     }
+
+  //     let filteredData = (this.original_data.filter((item) => {
+  //       return item.info.category == type
+  //     })
+  //     )
+
+  //     filteredData.map(item => {
+  //       this.productArr.push(item); 
+  //     })
+
+
+  //   }
+  //   if (!el.target.checked) {
+  //     this.productArr = this.productArr.filter(item =>
+  //       item.info.category != type);
+  //   }
+  //   // console.log(this.productArr, "finalll");
+
+  //   if (this.productArr.length === 0)
+  //   {
+  //     this.productArr = this.original_data;
+  //     this.i = 0;
+  //   }
+  // }
+
+  // clear() {
+  //   this.productArr = this.original_data;
+
+
+
+  // }
+
 
 }
 
