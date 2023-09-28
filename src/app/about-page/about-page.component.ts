d377a3c3df5de85e7fc20407209db81596a3e3d5
davinder22133 <s9718218@gmail.com>
2023-09-28 06:48:32 +0530

    updated setting component

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -8,29 +8,5 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./about-page.component.css']
 })
 export class AboutPageComponent {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    // autoplay: true,
-    navSpeed: 700,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1
-      },
-      400: {
-        items: 1
-      },
-      740: {
-        items: 1
-      },
-      940: {
-        items: 1
-      }
-    },
-    nav: false
-  }
+
 }
