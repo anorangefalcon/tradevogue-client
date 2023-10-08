2019f589f3f09f34266fc91595001a46bf9bc639
davinder22133 <s9718218@gmail.com>
2023-10-08 18:26:04 +0530

    updated signup page

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { passwordStrengthValidator, matchPasswordValidator,usernameValidator } from '../validators'; // Adjust the path to the correct location
+import { passwordStrengthValidator, matchPasswordValidator, usernameValidator } from '../validators'; // Adjust the path to the correct location
 import { CookieService } from 'ngx-cookie-service';
 import { UserDataService } from '../user-data.service';
+import { UtilsModule } from 'src/app/utils/utils.module';
+import { FetchDataService } from 'src/app/shared/services/fetch-data.service';
+
 // import { UserDataService } from '../user-data.service';
 
 @Component({
@@ -12,34 +15,35 @@ import { UserDataService } from '../user-data.service';
 })
 export class SignUpComponent {
 
-signupForm:FormGroup;
-users = [];
-isFormSubmitted: boolean = false;
-password:string='password';
-confirmPassword:string='password';
-showPassword:boolean=false;
-showPassword2:boolean=false;
-constructor(private fb:FormBuilder, private cookie : CookieService, private userData:UserDataService){
+  signupForm: FormGroup;
+  users = [];
+  isFormSubmitted: boolean = false;
+  password: string = 'password';
+  confirmPassword: string = 'password';
+  showPassword: boolean = false;
+  showPassword2: boolean = false;
+  constructor(private fb: FormBuilder, private cookie: CookieService, private userData: UserDataService, private backendURLs: UtilsModule, private fetchDataService: FetchDataService) {
 
-   this.userData.getData().subscribe((data:any)=>{
+    this.userData.getData().subscribe((data: any) => {
 
-    this.users=data;
-    console.log(this.users);
+      this.users = data;
+      console.log(this.users);
     });
-  
 
-  this.signupForm= fb.group(
 
-    
-    {
-      username:fb.control('',[Validators.required,usernameValidator]),
-      email:fb.control('',[Validators.email,Validators.required]),
-      password:fb.control('',[Validators.required,Validators.minLength(8), passwordStrengthValidator]),
-      confirmPassword: fb.control('',[Validators.required,(control: any) => matchPasswordValidator(control, this.signupForm)])
-      
-    });   
-    
-  
+    this.signupForm = fb.group(
+
+
+      {
+        firstname: fb.control('', [Validators.required]),
+        lastname: fb.control('', [Validators.required]),
+        email: fb.control('', [Validators.email, Validators.required]),
+        password: fb.control('', [Validators.required, Validators.minLength(8), passwordStrengthValidator]),
+        confirmPassword: fb.control('', [Validators.required, (control: any) => matchPasswordValidator(control, this.signupForm)])
+
+      });
+
+
   }
 
 
@@ -50,44 +54,37 @@ constructor(private fb:FormBuilder, private cookie : CookieService, private user
 
 
   // ON SUBMIT METHOD
-onSubmit() {
-  this.isFormSubmitted = true;
-  if (this.isFormSubmitted) {
-    if (this.signupForm.valid) {
-      const signupDetails = JSON.stringify(this.signupForm.value);
-      const storedSignupDetails = this.cookie.get('signupDetails');
-      let signupDetailsArray = [];
-      if (storedSignupDetails) {
-        signupDetailsArray = JSON.parse(storedSignupDetails);
+  async onSubmit() {
+    if (this.signupForm.invalid) return;
+    try {
+      const body = {
+        name: { firstname: this.signupForm.get('firstname')?.value, lastname: this.signupForm.get('lastname')?.value },
+        email: this.signupForm.get('email')?.value,
+        password: this.signupForm.get('password')?.value
       }
-      signupDetailsArray.push(signupDetails);
-      this.cookie.set('signupDetails', JSON.stringify(signupDetailsArray));
-      // console.log("signupDetails", signupDetails);
+
+      let data = await this.fetchDataService.httpPost(this.backendURLs.URLs.signupUrl, body);
     }
+
+    catch (error) {
+
+    }
+
+
+
+
   }
 
-  // Retrieve 
-  const storedSignupDetails = this.cookie.get('signupDetails');
-  if (storedSignupDetails) {
-    const signupDetailsArray = JSON.parse(storedSignupDetails);
-    for (const signupDetails of signupDetailsArray) {
-      const parsedSignupDetails = JSON.parse(signupDetails);
-      const username = parsedSignupDetails.username;
-      console.log('Username:', username);
-    }
-  }
-}
 
-
-  toggle_password(el:any){
-    if(el==1){
-      this.showPassword=!this.showPassword;
-      this.password=this.showPassword ? 'text':'password';
+  toggle_password(el: any) {
+    if (el == 'Password') {
+      this.showPassword = !this.showPassword;
+      this.password = this.showPassword ? 'text' : 'password';
     }
-   else{
-    this.showPassword2=!this.showPassword2;
-    this.confirmPassword=this.showPassword2 ? 'text':'password';
-   }
+    else {
+      this.showPassword2 = !this.showPassword2;
+      this.confirmPassword = this.showPassword2 ? 'text' : 'password';
+    }
 
   }
 
