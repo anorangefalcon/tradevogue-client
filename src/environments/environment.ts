54fa1bee085cc7ca31b9bcd51aa9be2465304170
anorangefalcon <priyanshudhall2001@gmail.com>
2023-12-05 12:58:25 +0530

    vercel route fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   production: false,
   // BackendURL:'http://localhost:1000',
   // BackendURL:'https://tradevogue-backend.onrender.com',
-  BackendURL:'https://e-commerce-backend-virid-one.vercel.app',
+  BackendURL:'https://e-commerce-backend-virid-one.vercel.app/api',
 
   Pincode:'http://54.157.29.156:4000',
   vapidKeyNotification: 'BPgBPO552gWCPJ_rUhzgn02bC3EFAIh1EWhlyib11X58vriYlQXmqeGX9_NJ8Z1h8KjtIDpstdWTgFuC01pdFbw'
